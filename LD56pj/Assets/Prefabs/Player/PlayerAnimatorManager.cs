bf1c5abda8907e49aabf8e57c6639ec767781fa6
entropy622 <1450431082@qq.com>
2024-10-05 18:20:48 +0800

    PlayerAnimator

--- LD56pj/Assets/Prefabs/Player/PlayerAnimatorManager.cs
+++ LD56pj/Assets/Prefabs/Player/PlayerAnimatorManager.cs
@@ -5,6 +5,8 @@ using QFramework;
 using UnityEditor;
 using UnityEngine;
 
+// SpeedMode: 0:Idle, 1:Walk, 2 Run;
+
 public class PlayerAnimatorManager : MonoSingleton<PlayerAnimatorManager>
 {
     public Animator playerAnimator;
@@ -12,7 +14,23 @@ public class PlayerAnimatorManager : MonoSingleton<PlayerAnimatorManager>
     public float crouchSpeed;//Speed when crouching
     public float runSpeed; //If speed is greater than runSpeed ,switch to tun;
     public float dashTime;*/
-    
+
+    public void SwitchToIdle()
+    {
+        playerAnimator.SetInteger("SpeedModeInt",0);
+    }
+    public void SwitchToWalk()
+    {
+        playerAnimator.SetInteger("SpeedModeInt",1);
+    }
+    public void SwitchToRun()
+    {
+        playerAnimator.SetInteger("SpeedModeInt",2);
+    }
+    public void SwitchToDash() //由于是一次性的动作，所以不需要切换状态
+    {
+        playerAnimator.CrossFade("Dash",0);
+    }
     public void ChangeCrouchState(bool state)
     {
         playerAnimator.SetBool("Crouch",state);
@@ -21,12 +39,13 @@ public class PlayerAnimatorManager : MonoSingleton<PlayerAnimatorManager>
     {
         playerAnimator.SetBool("Lift",state);
     }
+    public void ChangePushState(bool state)
+    {
+        playerAnimator.SetBool("Push",state);
+    }
     public void ChangeJumpState(bool state)
     {
         playerAnimator.SetBool("Jump",state);
     }
-    public void SwitchToDash() //由于是一次性的动作，所以不需要切换状态
-    {
-        playerAnimator.CrossFade("Dash",0);
-    }
+
 }
