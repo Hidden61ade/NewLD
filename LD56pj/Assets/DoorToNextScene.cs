aeec4c21acc77066efd65872bd04d7701d6e531b
hidden61ade <mclarenfang@icloud.com>
2024-10-06 18:54:08 +0800

    现在c1进门可以到c2

---
+++ LD56pj/Assets/DoorToNextScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kuchinashi.SceneControl;
+
+public class DoorToNextScene : MonoBehaviour
+{
+    public Transform player; // 角色的Transform
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // 检查碰撞的对象是否是player
+        if (other.transform == player)
+        {
+            // 调用SceneControl中的方法来加载下一个场景
+            Debug.Log("Player entered the door.");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("chapter2");
+        }
+    }
+}
