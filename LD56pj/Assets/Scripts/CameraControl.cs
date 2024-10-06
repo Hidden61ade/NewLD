52e673eb284edb6c0720b3d5686d7ba895948d8e
iaald <the_iaald@163.com>
2024-10-06 16:40:49 +0800

    Camera Shake

--- LD56pj/Assets/Scripts/CameraControl.cs
+++ LD56pj/Assets/Scripts/CameraControl.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using System.Threading.Tasks;
+using MoonSharp.VsCodeDebugger.SDK;
+using System;
 
 public class CameraControl : MonoBehaviour
 {
     public static Transform player; // 角色的Transform
     private CinemachineVirtualCamera virtualCamera;
+    public NoiseSettings noiseSettings;
     private CinemachineFramingTransposer framingTransposer;
+    public bool isShaking = false;
 
-    public void SetCameraFollow(Transform player){
+    public void SetCameraFollow(Transform player)
+    {
         // 获取Cinemachine虚拟摄像机组件
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
         if (virtualCamera != null)
@@ -22,5 +28,32 @@ public class CameraControl : MonoBehaviour
 
         // 设置角色在屏幕中的位置，偏左
         framingTransposer.m_ScreenX = 0.3f; // 0.3表示角色在屏幕左侧30%的位置
+    }
+
+    void CameraShake(float duration = 1f)
+    {
+        StartCoroutine(IENoise(duration, true));
+    }
+    
+    [Obsolete]
+    void CameraShakeUntil()
+    {
+        //持续修改isShaking的值
+        StartCoroutine(IENoise(1, false));
+    }
+
+    IEnumerator IENoise(float duration, bool isTimeControlled)
+    {
+        CinemachineBasicMultiChannelPerlin noise = virtualCamera.AddCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        noise.m_NoiseProfile = noiseSettings;
+        if (isTimeControlled)
+        {
+            yield return new WaitForSeconds(duration);
+        }
+        else
+        {
+            yield return new WaitUntil(() => { return !isShaking; });
+        }
+        virtualCamera.DestroyCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 }
