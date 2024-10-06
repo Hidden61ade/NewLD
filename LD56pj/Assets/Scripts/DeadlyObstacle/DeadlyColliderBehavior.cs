71954948cb7bb37f369ba2e3b4569fd9dd52665e
entropy622 <1450431082@qq.com>
2024-10-06 12:54:04 +0800

    致死类物体脚本

---
+++ LD56pj/Assets/Scripts/DeadlyObstacle/DeadlyColliderBehavior.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using QFramework;
+using UnityEngine;
+
+public class DeadlyColliderBehavior : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TypeEventSystem.Global.Send<OnPlayerDiedEvents>();
+        }
+    }
+}
