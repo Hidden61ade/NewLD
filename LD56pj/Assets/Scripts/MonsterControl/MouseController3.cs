303d01e7e07288603dc12ce38404cf183292dd2a
entropy622 <1450431082@qq.com>
2024-10-07 12:40:00 +0800

    Mouse

---
+++ LD56pj/Assets/Scripts/MonsterControl/MouseController3.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using QFramework;
+using UnityEngine;
+
+public class MouseController3 : MonoSingleton<MouseController3>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
