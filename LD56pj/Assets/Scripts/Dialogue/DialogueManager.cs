df06773546b11f89d337dac08d3e4f34e1dcef25
entropy622 <1450431082@qq.com>
2024-10-08 00:36:14 +0800

    mouse animation， dialogue

--- LD56pj/Assets/Scripts/Dialogue/DialogueManager.cs
+++ LD56pj/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -10,12 +10,7 @@ public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance;
     public TextMeshProUGUI contentText;
-    //public TextMeshProUGUI playerNameText;
     public GameObject talkPanel;
-    //public SpriteLib spriteLib;
-    //public float brightnessWhenNotSpeak = 0.6f;
-    //public Image leftCharacterImg;
-    //public Image rightCharacterImg;
     public Image backgroundImage;
     private int _remainDialogueNode;
     private int _dialogueIndex;
@@ -67,7 +62,7 @@ public class DialogueManager : MonoBehaviour
     }
     public void PutDialogue(Dialogue dialogue)//放置对话段落
     {
-        talkPanel.SetActive(true);
+        SetDialogueState(true);
         Init();
         _remainDialogueNode = dialogue.DialogueNodes.Length;
         _dialogueNodes = dialogue.DialogueNodes;
@@ -78,7 +73,7 @@ public class DialogueManager : MonoBehaviour
     {
         if (_remainDialogueNode<=0)
         {
-            talkPanel.SetActive(false);
+            SetDialogueState(false);
             return;
         }
         
@@ -90,56 +85,19 @@ public class DialogueManager : MonoBehaviour
         {
             backgroundImage.sprite = dialogueNode.PNGSprite;
         }
-        /*if (!_charactersIsFull)
-        {
-            //set the character on talk panel
-            //第一个出现在对话内的角色讲放在对话框右边,第二个放在左边
-            if (_remainCharacterSpace == 2)
-            {
-                _remainCharacterSpace--;
-                rightCharacterImg.enabled = true;
-                rightName = dialogueNode.playerName;
-                rightCharacterImg.sprite = spriteLib.GetSprite(dialogueNode.playerName);
-            }
-            else if (_remainCharacterSpace==1)
-            {
-                _remainCharacterSpace--;
-                leftCharacterImg.enabled = true;
-                leftName = dialogueNode.playerName;
-                leftCharacterImg.sprite = spriteLib.GetSprite(dialogueNode.playerName);
-                _charactersIsFull = true;
-            }
-        }
-        //Set the text components
-        contentText.text = dialogueNode.content;
-        playerNameText.text = dialogueNode.playerName;
-        
-        //让不在对话时候的角色变灰
-        if (!leftName.Equals(dialogueNode.playerName))
-        {
-            SetBrightNess(brightnessWhenNotSpeak,leftCharacterImg);
-        }
-        else
-        {
-            SetBrightNess(1,leftCharacterImg);
-        }
-        
-        if (!rightName.Equals(dialogueNode.playerName))
-        {
-            SetBrightNess(brightnessWhenNotSpeak,rightCharacterImg);
-        }
-        else
-        {
-            SetBrightNess(1,rightCharacterImg);
-        }*/
     }
 
-    /*private void SetBrightNess(float brightness,Image img)
+    public void SetDialogueState(bool state)
     {
-        Color color = img.color;
-        color.r = brightness;
-        color.g = brightness;
-        color.b = brightness;
-        img.color = color;
-    }*/
+        talkPanel.SetActive(state);
+        if (state==true)
+        {
+            Time.timeScale = 0;
+        }
+
+        if (state==false)
+        {
+            Time.timeScale = 1;
+        }
+    }
 }
