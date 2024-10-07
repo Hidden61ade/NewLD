3306fd8040fec5f1007a09e49e6d52d5f80b5d6f
iaald <the_iaald@163.com>
2024-10-07 10:11:07 +0800

    Update StatedPara

--- LD56pj/Assets/Scripts/Bases/StateEnemyTest.cs
+++ LD56pj/Assets/Scripts/Bases/StateEnemyTest.cs
@@ -10,18 +10,33 @@ using UnityEngine;
 public class StateEnemyTest : StatedParameter<StateEnemyTest>
 {
     public int a = 1;
-    [StatedPara]public int b = 2;
-    [StatedPara]public int c = 3;
-    [StatedPara]public bool d = false;
-    private void Start() {
+    [StatedPara] public int b = 2;
+    [StatedPara] public int c = 3;
+    [StatedPara] public bool d = false;
+    [StatedPara]
+    public Vector3 A
+    {
+        set
+        {
+            transform.position = value;
+        }
+        get
+        {
+            return transform.position;
+        }
+    }
+    private void Start()
+    {
         LookPara(this);
-        TypeEventSystem.Global.Register<OnStateChangeEvent>(e=>{
+        TypeEventSystem.Global.Register<OnStateChangeEvent>(e =>
+        {
             ResetPara(this);
         });
         StartCoroutine(TESTER());
     }
 
-    IEnumerator TESTER(){
+    IEnumerator TESTER()
+    {
         yield return new WaitForSeconds(10);
         TypeEventSystem.Global.Send<OnStateChangeEvent>();
         yield return new WaitForSeconds(5);
--- LD56pj/Assets/Scripts/Bases/StatedParameter.cs
+++ LD56pj/Assets/Scripts/Bases/StatedParameter.cs
@@ -11,18 +11,36 @@ public class StatedParameter<T> : MonoBehaviour
     public StatedParameter()
     {
     }
+
     /// <summary>
     /// 获得StatedPara数据快照
     /// </summary>
     /// <param name="var">被控制对象</param>
     protected void LookPara(T var)
     {
+        // 获取字段
         var FIs = typeof(T).GetFields();
         foreach (var item in FIs)
         {
             if (item.IsDefined(typeof(StatedPara), false))
             {
-                if(fieldInfos.ContainsKey(item.Name)){
+                if (fieldInfos.ContainsKey(item.Name))
+                {
+                    fieldInfos[item.Name] = item.GetValue(var);
+                    continue;
+                }
+                fieldInfos.Add(item.Name, item.GetValue(var));
+            }
+        }
+
+        // 获取属性
+        var PIs = typeof(T).GetProperties();
+        foreach (var item in PIs)
+        {
+            if (item.IsDefined(typeof(StatedPara), false) && item.CanRead && item.CanWrite)
+            {
+                if (fieldInfos.ContainsKey(item.Name))
+                {
                     fieldInfos[item.Name] = item.GetValue(var);
                     continue;
                 }
@@ -30,12 +48,14 @@ public class StatedParameter<T> : MonoBehaviour
             }
         }
     }
+
     /// <summary>
     /// 恢复上一次StatedPara数据快照
     /// </summary>
     /// <param name="var">被控制对象</param>
     public void ResetPara(T var)
     {
+        // 恢复字段
         var FIs = typeof(T).GetFields();
         foreach (var item in FIs)
         {
@@ -44,10 +64,20 @@ public class StatedParameter<T> : MonoBehaviour
                 item.SetValue(var, fieldInfos[item.Name]);
             }
         }
+
+        // 恢复属性
+        var PIs = typeof(T).GetProperties();
+        foreach (var item in PIs)
+        {
+            if (fieldInfos.ContainsKey(item.Name) && item.CanWrite)
+            {
+                item.SetValue(var, fieldInfos[item.Name]);
+            }
+        }
     }
 }
 
-[AttributeUsage(AttributeTargets.Field)]
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 public sealed class StatedPara : Attribute
 {
 }
