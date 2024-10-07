f7f35300b575dadac8fcff2eb284e0e6cf9fd53c
hidden61ade <mclarenfang@icloud.com>
2024-10-07 12:49:49 +0800

    第一关光照，背景，存档，现在差箱子

--- LD56pj/Assets/Scripts/GameProgress/UIManager.cs
+++ LD56pj/Assets/Scripts/GameProgress/UIManager.cs
@@ -24,6 +24,7 @@ public class UIManager : MonoSingleton<UIManager>
 
     private void Awake()
     {
+        DontDestroyOnLoad(gameObject);
         UICanvas = GameObject.Find("UICanvas").transform;
         mDeathMessage = UICanvas.Find("DeathMessage").gameObject;
         mDialoguePanel = UICanvas.Find("DialoguePanel").gameObject;
