b73b9032a33b3b7baa6273f9121522ba66704c2a
entropy622 <1450431082@qq.com>
2024-10-08 02:07:25 +0800

    对话的Trigger

---
+++ LD56pj/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public bool canTrigger = true;
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (canTrigger&&other.CompareTag("Player"))
+        {
+            canTrigger = false;
+            StartCoroutine(putDialogue());
+        }
+    }
+
+    public IEnumerator putDialogue()
+    {
+        yield return new WaitForSeconds(5f);
+        DialogueManager.Instance.PutDialogue(dialogue);
+    }
+}
