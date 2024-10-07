80242c3bb0f980b28c36a454319996cfd9982158
LoveCube700 <jkwang0401@gmail.com>
2024-10-07 09:22:56 +0800

    BoxBePushed预制体

--- LD56pj/Assets/Scripts/InterableObject/BoxPushed.cs
+++ LD56pj/Assets/Scripts/InterableObject/BoxPushed.cs
@@ -6,23 +6,43 @@ using UnityEngine;
 public class BoxPushed : MonoBehaviour
 {
     private Vector3 startPos;
+    private Rigidbody2D rb;
+    public bool isBePushed = false;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
     }
 
     // Update is called once per frame
     void Update()
     {
         TypeEventSystem.Global.Register<OnLevelResetEvent>(e => RestartBox()).UnRegisterWhenGameObjectDestroyed(gameObject);
+        BePushed();
     }
 
     private void RestartBox()
     {
         transform.position = startPos;
+    }
+
+    public void BePushed()
+    {
+        if(isBePushed)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;  // 解冻所有约束
+            rb.freezeRotation = true;
+        }
+        else
+        {
+            rb.freezeRotation = true;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
     }
 }
