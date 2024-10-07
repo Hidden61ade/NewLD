6f954ca8e745e4c37ceaaec262268e713b515a32
LoveCube700 <jkwang0401@gmail.com>
2024-10-07 22:00:36 +0800

    门开关加灯

--- LD56pj/Assets/Scripts/InterableObject/ButtonOfPressureSensitiveDoor.cs
+++ LD56pj/Assets/Scripts/InterableObject/ButtonOfPressureSensitiveDoor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ButtonOfPressureSensitiveDoor : MonoBehaviour
 {
@@ -8,11 +9,17 @@ public class ButtonOfPressureSensitiveDoor : MonoBehaviour
     public Sprite Off;
     public Sprite On;
 
+    public GameObject light;
+
+    Light2D light2d;
+
     private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        light2d = light.GetComponent<Light2D>();
+        light2d.color = new Color(255f / 255f, 33f / 255f, 20f / 255f);
     }
 
     private void Update()
@@ -20,10 +27,12 @@ public class ButtonOfPressureSensitiveDoor : MonoBehaviour
         if(isOpen)
         {
             spriteRenderer.sprite = On;
+            light2d.color = new Color(20f / 255f, 255f / 255f, 35f / 255f);
         }
         else
         {
             spriteRenderer.sprite = Off;
+            light2d.color = new Color(255f / 255f, 33f / 255f, 20f / 255f);
         }
     }
 
